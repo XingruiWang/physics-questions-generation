6f9dcfc296a226d1e2689369d6a3a5ffc2fe97db
Angtian Wang <angtianwang@gmail.com>
2024-04-15 00:08:36 -0400

    init

---
+++ generate.sh
@@ -0,0 +1,5 @@
+python generate_questions.py \
+    --input_scene_file CLEVR_v1.0/scenes/CLEVR_val_scenes.json \
+    --output_questions_file output/sample_questions.json \
+    --scene_start_idx 0 \
+    --num_scenes 1
